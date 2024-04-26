1aaa20a8d54e821a130db5fb940d0d5fdf94c0c1
happyd0nut <juliaalau2@gmail.com>
2024-04-26 09:28:04 -0700

    classes made

--- game.py
+++ game.py
@@ -1,5 +1,32 @@
+import mediapipe as mp
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
+import cv2
+import random
 import numpy as np
+import time
+
+# Library Constants
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+DrawingUtil = mp.solutions.drawing_utils
 
 
+
+class Game():
+    def __init__(self, mode):
+        
+        self.mode = mode
+        self.targets = []
+        self.score = 0
+
+        options = PoseLandmarkerOptions(
+        base_options = BaseOptions(model_asset_path="data/pose_landmarker.task", 
+                                   num_poses="1")
+        running_mode = VisionRunningMode.VIDEO)
+        self.video = cv2.VideoCapture(1)
+
+    def __main__():
+        print()
---
+++ target.py
@@ -0,0 +1,19 @@
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import cv2
+import random
+import numpy as np
+import time
+
+
+class Target():
+    def __init__(self, screen_width=600, screen_height=400, mode):
+        
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.
+        self
+
+    def __main__():
+        print()
