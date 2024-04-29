a87410aac8b4ff9d2f976821d4ae51b99fba2311
happyd0nut <juliaalau2@gmail.com>
2024-04-29 14:58:03 -0700

    detect 2 bodies

--- game.py
+++ game.py
@@ -23,15 +23,19 @@ class Game():
         self.targets = []
         self.score = 0
         
+        # Create PoseLandmarker detector
         base_options = BaseOptions(model_asset_path="data/pose_landmarker_full.task")
-        options = PoseLandmarkerOptions(base_options = base_options, 
+        options = PoseLandmarkerOptions(base_options = base_options, num_poses = 2,
                                         output_segmentation_masks = True)
         self.detector = PoseLandmarker.create_from_options(options)
 
+        # Start video
         self.video = cv2.VideoCapture(1)
 
 
     def draw_landmarks(self, image, detection_result):
+        
+        # Get list of poses detected
         pose_landmarks_list = detection_result.pose_landmarks
 
         for idx in range(len(pose_landmarks_list)):
@@ -44,20 +48,33 @@ class Game():
             DrawingUtil.draw_landmarks(image,
                                         pose_landmarks_proto,
                                         solutions.pose.POSE_CONNECTIONS,
-                                        DrawingUtil.get_default_pose_landmarks_style())
+                                        solutions.drawing_styles.get_default_pose_landmarks_style())
+
+
+    def label_structures(self, image, detection_result):
+        pass
 
 
     def run(self):
+
         while self.video.isOpened():
+            
+            # Get frame of video feed
             frame = self.video.read()[1]
 
+            # Turn to RGB and flip image
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image = cv2.flip(image, 1)
 
+            # Use PoseLandmarker to detect poses
             to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-            # results = self.detector.detect(to_detect)
+            results = self.detector.detect(to_detect)
 
-            image =cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # Draw landmarks on poses
+            self.draw_landmarks(image, results)
+
+            # Display image
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             cv2.imshow("Pose Tracking", image)
 
             if cv2.waitKey(50) & 0xFF == ord('q'):
